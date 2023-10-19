1e86899a545d8f3365beaf3f47d1ac51f4584314
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-19 07:02:54 +0530

    feat: add navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <div className="nav-container">
+            <Link to="/">
+              <img src="" alt="logo" className="logo-img" />
+            </Link>
+            <div className="nav-links">
+              <Link to="/">categories</Link>
+              <Link to="/">lamps</Link>
+              <Link to="/">furniture</Link>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
