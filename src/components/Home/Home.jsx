806143c0c3909c20b189f6c7fc9753c088048f0d
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-19 23:10:28 +0530

    feat: add footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../Hero/Hero";
 import Products from "../Products/Products";
 import Banner from "../Banner/Banner";
 import BannerReverse from "../Banner/BannerReverse";
+import Footer from "../Footer/Footer";
 import Banner1 from "../../images/banner/banner1.jpg";
 import Banner2 from "../../images/banner/banner2.jpg";
 
@@ -20,6 +21,7 @@ const Home = () => {
         text=" Products are all made to standard sizes so that you can mix and match them freely."
         img={Banner2}
       />
+      <Footer />
     </>
   );
 };
