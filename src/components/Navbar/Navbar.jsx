e977562daa15efc8250efc65f565e7f98567856c
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-20 11:32:42 +0530

    feat: add navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
     <>
       <nav className="navbar">
         <div className="container">
-          <div className="nav-container">
+          <div className={`nav-container ${sticky ? "cont-sticky" : ""}`}>
             <Link to="/">
               <img src="" alt="logo" className="logo-img" />
             </Link>
