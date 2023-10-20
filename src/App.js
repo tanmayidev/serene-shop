a4a21394151b4a365a06ab604b944dfdee4499ad
tanmayidev <tanmayi.d.99@gmail.com>
2023-10-20 17:59:50 +0530

    feat: add category page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Categories from "./pages/Categories";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </>
   );
